9ac203b1cc59e254b4f1ea5e649b44d802cbcd66
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-12-30 18:54:06 -0300

    estructura inicial finalizada

--- PARKE-Landing-Page/Data/Repositories/UserRepository.cs
+++ PARKE-Landing-Page/Data/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using PARKE_Landing_Page.Data.Services;
+using PARKE_Landing_Page.Models.Entities;
 
 namespace PARKE_Landing_Page.Data.Repositories
 {
@@ -11,6 +12,34 @@ namespace PARKE_Landing_Page.Data.Repositories
             _context = context;
         }
 
+        public User Add(User user)
+        {
+            _context.Set<User>().Add(user);
+            _context.SaveChanges();
+            return user;
+        }
 
+        public void Update(User user)
+        {
+            _context.SaveChanges();
+        }
+
+        public void Delete(User user)
+        {
+            _context.Remove(user);
+            _context.SaveChanges();
+        }
+
+        public void SaveChanges() { _context.SaveChanges(); }
+
+        public User? GetById(int id)
+        {
+            return _context.Users.FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
     }
 }
--- PARKE-Landing-Page/obj/Debug/net8.0/PARKE-Landing-Page.AssemblyInfo.cs
+++ PARKE-Landing-Page/obj/Debug/net8.0/PARKE-Landing-Page.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PARKE-Landing-Page")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7888aea4767df2a1ea0db3a8c3602acb34e2b837")]
 [assembly: System.Reflection.AssemblyProductAttribute("PARKE-Landing-Page")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PARKE-Landing-Page")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

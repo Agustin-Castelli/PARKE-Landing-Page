1c8726974c74421c990c9d6bf4cacc02ab664e42
facundolgomez <facundolgomez87@gmail.com>
2025-01-01 22:52:21 -0300

    adding gitignore file

--- PARKE-Landing-Page/Models/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PARKE_Landing_Page.Models.Entities
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }
-    }
-}
--- PARKE-Landing-Page/Services/DTOs/UserRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PARKE_Landing_Page.Services.DTOs
-{
-    public class UserRequest
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}

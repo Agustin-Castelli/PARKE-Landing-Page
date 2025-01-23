73b1c14aeb6ff8b7d6069f0e90cb9f77beae184a
Gero <gerojbro@gmail.com>
2025-01-23 19:27:34 -0300

    creando las clases pt2

--- PARKE-Landing-Page/Models/Entities/Machine.cs
+++ PARKE-Landing-Page/Models/Entities/Machine.cs
@@ -2,5 +2,16 @@
 {
     public class Machine
     {
+        public int Id { get; set; } // Identificador único
+        public string Name { get; set; } // Nombre de la máquina (ejemplo: "Embolsadora de bolsas")
+        public string? Type { get; set; } // Tipo de máquina (ejemplo: "Embolsadoras", "Cosedoras", etc.)
+        public string? Subtype { get; set; } // Subtipo dentro del tipo (ejemplo: "Boca abierta", "Valvuladas", etc.)
+        public string Model { get; set; } // Modelo específico (ejemplo: "EP5B-TC", "GK35")
+        public string Description { get; set; } // Descripción breve opcional
+        public string FileName { get; set; }
+        public List<string> MediaContent { get; set; }
+
+
+        public ICollection<ClientDetail> ClientDetails { get; set; } = new List<ClientDetail>();
     }
 }

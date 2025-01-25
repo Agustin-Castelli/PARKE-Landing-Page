321ffd27451e3524c420cfc5af25eee88a0a5dad
facundolgomez <facundolgomez87@gmail.com>
2025-01-24 21:44:26 -0300

    client request

---
+++ PARKE-Landing-Page/Services/DTOs/ClientRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PARKE_Landing_Page.Services.DTOs
+{
+    public class ClientRequest
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}

c240fa3bffea0a4e940785ee9672f56c4d086c9d
facundolgomez <facundolgomez87@gmail.com>
2025-01-28 17:29:51 -0300

    new interfaces

---
+++ PARKE-Landing-Page/Services/Interfaces/IAuthenticationServiceClient.cs
@@ -0,0 +1,9 @@
+﻿using PARKE_Landing_Page.Services.DTOs;
+
+namespace PARKE_Landing_Page.Services.Interfaces
+{
+    public interface IAuthenticationServiceClient
+    {
+        string Authenticate(AuthenticationRequestClient authenticationRequest);
+    }
+}

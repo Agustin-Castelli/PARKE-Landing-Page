3b5df70bd3642e0bcff3a7024fd2af9cfe74eb32
facundolgomez <facundolgomez87@gmail.com>
2025-01-28 17:29:17 -0300

    new requests for the two entities

---
+++ PARKE-Landing-Page/Services/DTOs/AuthenticationRequestAdmin.cs
@@ -0,0 +1,8 @@
+﻿namespace PARKE_Landing_Page.Services.DTOs
+{
+    public class AuthenticationRequestAdmin
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ PARKE-Landing-Page/Services/DTOs/AuthenticationRequestClient.cs
@@ -0,0 +1,8 @@
+﻿namespace PARKE_Landing_Page.Services.DTOs
+{
+    public class AuthenticationRequestClient
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}

7d911921da9d2650b36c02ac09f419d89f3f8d94
facundolgomez <facundolgomez87@gmail.com>
2025-01-31 16:54:14 -0300

    changing ContactFormRequest properties

--- PARKE-Landing-Page/Services/DTOs/ContactFormRequest.cs
+++ PARKE-Landing-Page/Services/DTOs/ContactFormRequest.cs
@@ -2,9 +2,12 @@
 {
     public class ContactFormRequest
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Name { get; set; }       // Se mantiene igual, ya que "Name" es correcto
+        public string Email { get; set; }      // También se mantiene igual, "Email" es correcto
+        public string Company { get; set; }    // Cambié "Subject" por "Company" para que coincida con el campo "Empresa"
+        public string Phone { get; set; }      // Cambié "Message" por "Phone" para coincidir con el campo "Teléfono"
         public string Subject { get; set; }
-        public string Message { get; set; }
+        public string Message { get; set; }    // "Message" se mantiene igual, porque coincide con el campo "Mensaje"
     }
+
 }

8e231202f14cbb2422dbaa6a608ee97e83b667e9
facundolgomez <facundolgomez87@gmail.com>
2025-04-17 00:56:04 -0300

    Implement IHashingService in HashingService

---
+++ PARKE-Landing-Page/Services/HashingService.cs
@@ -0,0 +1,19 @@
+﻿using PARKE_Landing_Page.Services;
+using PARKE_Landing_Page.Services.Interfaces;
+
+namespace PARKE_Landing_Page.Services
+
+{
+    public class HashingService : IHashingService
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string hash )
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}

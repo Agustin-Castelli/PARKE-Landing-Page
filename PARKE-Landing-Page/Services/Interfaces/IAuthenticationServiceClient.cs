73644373b41bcbc9d3b2fcd14fe90b13ddabc4d9
facundolgomez <facundolgomez87@gmail.com>
2025-05-12 15:47:11 -0300

    Add ResetPassword and GenerateRecoveryToken methods

--- PARKE-Landing-Page/Services/Interfaces/IAuthenticationServiceClient.cs
+++ PARKE-Landing-Page/Services/Interfaces/IAuthenticationServiceClient.cs
@@ -5,5 +5,7 @@ namespace PARKE_Landing_Page.Services.Interfaces
     public interface IAuthenticationServiceClient
     {
         string Authenticate(AuthenticationRequestClient authenticationRequest);
+        bool ResetPassword(string token, string newPassword);
+        string GenerateRecoveryToken(string email);
     }
 }

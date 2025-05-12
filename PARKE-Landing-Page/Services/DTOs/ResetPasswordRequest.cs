acaf738725b98efbe9032cc2c825d62415a40200
facundolgomez <facundolgomez87@gmail.com>
2025-05-12 15:31:02 -0300

    Add new Dto to reset password

---
+++ PARKE-Landing-Page/Services/DTOs/ResetPasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PARKE_Landing_Page.Services.DTOs
+{
+    public class ResetPasswordRequest
+    {
+        public string Token { get; set; }        
+        public string NewPassword { get; set; }  
+        public string ConfirmPassword { get; set; }
+    }
+}

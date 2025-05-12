66a3e23e9154a89dd71e673d56aa2501be02ced2
facundolgomez <facundolgomez87@gmail.com>
2025-05-12 15:45:07 -0300

    Add SendRecoveryLinkToUser method

--- PARKE-Landing-Page/Services/Interfaces/IEmailService.cs
+++ PARKE-Landing-Page/Services/Interfaces/IEmailService.cs
@@ -6,6 +6,6 @@ namespace PARKE_Landing_Page.Services.Interfaces
     {
         public void SendContactEmail(ContactFormRequest form);
         public void SendUserConfirmationEmail(string userEmail, string userName);
-        void SendPasswordRecoveryRequestToCompany(string userEmail);
+        void SendRecoveryLinkToUser(string email, string link);
     }
 }
